558a53045c63d82bb3bbd522a453777174c85e23
GabrielBN123 <gabriel.nogueira@senaimt.edu.br>
2021-05-16 21:18:05 -0400

    Update pulpito.blade.php

--- resources/views/painel/pulpito.blade.php
+++ resources/views/painel/pulpito.blade.php
@@ -3,65 +3,80 @@
 @section('title', 'Painel')
 
 @section('content')
-    <div class="accordion mx-5 mt-2" id="accordionPanelsStayOpenExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button " type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                    aria-controls="panelsStayOpen-collapseOne">
-                    Apresentação de Visitante
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                    @foreach ($formulario as $item)
-                        <div class="alert alert-light alert-dismissible border border-primary fade show">
-                                {{ $item->apresentacao }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    @endforeach
+    @php
+    $forms = [
+        'Apresentação de Visitante' => 'apresentacao',
+        'Avisos' => 'aviso',
+        'Pedidos de Oração' => 'pedidoOracao',
+        'Felicitações' => 'Felicitacao',
+        'Pedidos de Louvor' => 'pedidoLouvor',
+        'Ação de Graças' => 'acaoGracas',
+        'Apresentação de Recém Nascidos' => 'apresentacaoRN',
+        'Pedido de Comunhão' => 'pedidoComunhao',
+        'Carta de Apresentação' => 'cartaApresentacao',
+    ];
+    @endphp
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+        @foreach ($forms as $title => $id)
+            <div class="accordion-item">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-{{ $id }}">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-{{ $id }}" aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapseOne">
+                            {{ $title }}
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-{{ $id }}" class="accordion-collapse collapse show"
+                        aria-labelledby="panelsStayOpen-{{ $id }}">
+                        @foreach ($formulario as $content)
+                            <div class="accordion-body">
+                                {{ $content->$id }}
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
-                    aria-controls="panelsStayOpen-collapseTwo">
-                    Accordion Item #2
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingTwo">
-                <div class="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
-                    plugin adds the appropriate classes that we use to style each element. These classes control the overall
-                    appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with
-                    custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go
-                    within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
-            </div>
-        </div>
+        @endforeach
+    </div>
 
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
-                    aria-controls="panelsStayOpen-collapseThree">
-                    Accordion Item #3
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
-                aria-labelledby="panelsStayOpen-headingThree">
-                <div class="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse
-                    plugin adds the appropriate classes that we use to style each element. These classes control the overall
-                    appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with
-                    custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go
-                    within the <code>.accordion-body</code>, though the transition does limit overflow.
-                </div>
+    {{-- <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo">
+                Accordion Item #2
+            </button>
+        </h2> --}}
+    {{-- <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingTwo">
+            <div class="accordion-body">
+                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
+                plugin adds the appropriate classes that we use to style each element. These classes control the overall
+                appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go
+                within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+        </div> --}}
+    </div>
+
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseThree">
+                Accordion Item #3
+            </button>
+        </h2>
+        <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
+            aria-labelledby="panelsStayOpen-headingThree">
+            <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse
+                plugin adds the appropriate classes that we use to style each element. These classes control the overall
+                appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go
+                within the <code>.accordion-body</code>, though the transition does limit overflow.
             </div>
         </div>
+    </div>
     </div>
 @endsection

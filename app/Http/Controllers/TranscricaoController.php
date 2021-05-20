19bd52fb5dbe84ea508b667866d792418dc27343
Gabriel <gabrielnogueira@seplag.mt.gov.br>
2021-05-20 16:59:05 -0400

    inicio transcricao

--- app/Http/Controllers/TranscricaoController.php
+++ app/Http/Controllers/TranscricaoController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class TranscricaoController extends Controller
 {
     public function index(){
-        return view('painel.index',[
+        return view('painel.transcricao',[
             'rotaindex' => route('painel.transcricao.index')
         ]);
     }
---
+++ resources/views/painel/transcricao.blade.php
@@ -0,0 +1,36 @@
+@extends('template.template')
+
+@section('title', 'Painel')
+
+@section('content')
+    <div class="accordion px-5 pt-3" id="accordionPanelsStayOpenExample">
+        @foreach ($forms as $title => $id)
+            <div class="accordion-item pb-1 bg-secondary">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo-{{ $id }}">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapseTwo-{{ $id }}" aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapseTwo-{{ $id }}">
+                            {{ $title }}
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseTwo-{{ $id }}" class="accordion-collapse collapse"
+                        aria-labelledby="panelsStayOpen-headingTwo-{{ $id }}">
+                        @foreach ($formulario as $content)
+                            <div class="accordion-body">
+                                @if ($content->$id != null)
+                                    <div class="alert alert-secondary bg-transparent alert-dismissible fade show" role="alert">
+                                        {{ $content->$id }}
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                @else
+                                    <div class="alert alert-info">Não há <strong>{{ $title }}</strong>.</div>
+                                @endif
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection

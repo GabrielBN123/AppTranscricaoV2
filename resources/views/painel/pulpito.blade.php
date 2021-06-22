971e9c66192f0b5e3a49d2a550289a47f09ba72e
GabrielBN123 <gabriel.nogueira@senaimt.edu.br>
2021-06-22 00:37:51 -0400

    att

--- resources/views/painel/pulpito.blade.php
+++ resources/views/painel/pulpito.blade.php
@@ -24,12 +24,12 @@
                                 <div class="accordion-body">
                                     @if (isset($content->texto))
                                         <div class="row">
-                                            <div class="col-11 alert-secondary bg-transparent alert-dismissible fade show fs-3"
+                                            <div class="col-11 alert-secondary bg-transparent alert-dismissible fade show fs-3 text"
                                                 role="alert">
                                                 {{ $content->texto }}
                                             </div>
                                             <div class="col-1">
-                                                <button type="button" class="btn btn-success rounded-circle">
+                                                <button type="button" class="btn btn-success rounded-circle btn_readed">
                                                     <i class="far fa-check-circle"></i>
                                                 </button>
                                             </div>
@@ -63,7 +63,12 @@
             </div>
         @endforeach
     </div>
-    {{-- @push('scripts')
-        <script src="{{ asset('js/pulpito.js') }}"></script>
-    @endpush --}}
+    @push('scripts')
+        <script>
+            $('.btn_readed').on('click', function (e) { 
+                var div = $(e.target).closest( 'div' );
+                div.prev('div').toggleClass( "readed" ) //.( '.text' ).toggleClass( "readed" )
+             });
+        </script>
+    @endpush
 @endsection
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -19,7 +19,7 @@
     @include('painel.componentes.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/all.min.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
